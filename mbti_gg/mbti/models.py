6d072f74f0049f8eb8b9f240d52bca9dd88d0b52
woohan100 <80763549+woohan100@users.noreply.github.com>
2022-03-07 11:15:15 +0900

    Created models: Mbti, User.

--- mbti_gg/mbti/models.py
+++ mbti_gg/mbti/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+class Mbti(models.Model):
+    mbti_id = models.CharField(max_length=4, primary_key=True)
+    desc = models.CharField(max_length=50)
+    profile = models.TextField()
+
--- mbti_gg/user/models.py
+++ mbti_gg/user/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.db.models import Q
+
 
 # Create your models here.
+class User(models.Model):
+    user_id = models.CharField(max_length=20, primary_key=True)
+    name = models.CharField(max_length=20, unique=True)
+    pwd = models.CharField(max_length=50)
+    mbti_id = models.ForeignKey('mbti.Mbti', on_delete=models.PROTECT, db_column='mbti_id')
+    birth_dt = models.DateField(null=True, blank=True)
+    gender = models.CharField(max_length=1)
+
+    class Meta:
+        constraints = [
+            models.CheckConstraint(check=Q(gender__in='M' or 'F'), name='gender')
+        ]
+
