aa1e130a346b3bb63711e1fcfd7b4b26881d0896
PHS <83255249+gus5963@users.noreply.github.com>
2022-03-16 14:31:12 +0900

    댓글기능

--- mbti_gg/hobby/urls.py
+++ mbti_gg/hobby/urls.py
@@ -4,5 +4,6 @@ from hobby import views
 urlpatterns = [
     path('', views.index, name='hobby_index'),
     path('like_btn/', views.like, name='hobby_like'),
-    path('like_submit/', views.rmd_submit, name='like-submit')
+    path('like_submit/', views.rmd_submit, name='like_submit'),
+    path('create_cmt/', views.create_cmt, name='create_cmt')
 ]
