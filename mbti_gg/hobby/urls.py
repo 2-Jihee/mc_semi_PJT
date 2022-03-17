fef0f15ef59da8c980e8df6d193e7c7cf348bfdb
PHS <83255249+gus5963@users.noreply.github.com>
2022-03-17 20:06:26 +0900

    comment delete btn

--- mbti_gg/hobby/urls.py
+++ mbti_gg/hobby/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.index, name='hobby_index'),
     path('like_btn/', views.like, name='hobby_like'),
     path('like_submit/', views.rmd_submit, name='like_submit'),
-    path('create_cmt/', views.create_cmt, name='create_cmt')
+    path('create_cmt/', views.create_cmt, name='create_cmt'),
+    path('cmt_del/', views.cmt_del, name='cmt_del')
 ]
