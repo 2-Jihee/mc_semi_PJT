02df38647c6f6008b18ccfee9c3dde2420330496
gyueun <rbdms20@naver.com>
2022-03-19 09:31:03 +0900

    updated movie index.html, cmt_create

--- mbti_gg/movie/urls.py
+++ mbti_gg/movie/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     # http://127.0.0.1:8000/movie/
     path('', views.index, name='movie_index'),
     path('like_btn/', views.like, name='movie_like'),
-    path('like_submit/', views.rmd_submit, name='like_submit'),
+    path('like_submit/', views.rmd_submit, name='movie_like_submit'),
     path('create_cmt/', views.create_cmt, name='movie_create_cmt'),
     path('cmt_del/', views.cmt_del, name='movie_cmt_del'),
 
