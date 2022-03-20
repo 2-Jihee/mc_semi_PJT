607612346c6c57e6f91a516084a4109a3c6d22fd
bronzesiuba <ehddms530@gmail.com>
2022-03-21 07:29:06 +0900

    Tour html

--- mbti_gg/tour/urls.py
+++ mbti_gg/tour/urls.py
@@ -1,9 +1,10 @@
-from django.urls import path
-from tour import views
+from django.urls import path, include
+from hobby import views
 
 urlpatterns = [
-    path('', views.index, name='tour_index'),
-    path('like_btn/', views.like, name='tour_like'),
+    path('', views.index, name='hobby_index'),
+    path('like_btn/', views.like, name='hobby_like'),
     path('like_submit/', views.rmd_submit, name='like_submit'),
     path('create_cmt/', views.create_cmt, name='create_cmt'),
+    path('cmt_del/', views.cmt_del, name='cmt_del')
 ]
