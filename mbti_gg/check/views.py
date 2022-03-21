15e876da9579f65e622064fad3101612067060bd
woohan100 <80763549+woohan100@users.noreply.github.com>
2022-03-21 10:14:05 +0900

    check.views.index initialized.

--- mbti_gg/check/views.py
+++ mbti_gg/check/views.py
@@ -1,8 +1,18 @@
-from django.shortcuts   import render, redirect
-from .models            import *
+from django.shortcuts import render, redirect
+from common.views import context_login, context_selected_mbti
+
 
 # Create your views here.
+def index(request):
+    print('>>> Check - Index')
 
-def index(request) :
-    print(">>>>>>>>>> check index")
-    return render(request, 'check/index.html')
+    # initialize the page
+    context = {
+        'title': 'MBTI Check',
+        'nav_link_active': 'check',
+    }
+    context_login(context, request)
+    context_selected_mbti(context, request)
+    
+    return render(request, 'check/index.html', context)
+
